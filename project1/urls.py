3e654cda3de0e2f362a21a6ecd8394cb2017e994
poojitha1234155 <poojithachitrada@gmail.com>
2025-12-06 18:44:55 +0530

    renamed file

--- project1/urls.py
+++ project1/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for project2 project.
+URL configuration for project1 project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.2/topics/http/urls/
@@ -19,4 +19,7 @@ from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+   
 ]
+
+
